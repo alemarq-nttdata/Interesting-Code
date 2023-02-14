cc6b07d7d027c3c47eb7ac79eee3774ec7a89233
Alejandro Marquez Aragones <amarqara@nttdata.com>
2023-02-14 11:22:43 +0100

    regex modificado

--- Javascript/regex.js
+++ Javascript/regex.js
@@ -1,10 +1,33 @@
-// Regex input number and decimals only, not string
-<input type="number" onkeypress="inpNum(event)" />
+/*
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Display a Number Field</h1>
 
-function inpNum(e) {
-  e = e || window.event;
-  var charCode = (typeof e.which == "undefined") ? e.keyCode : e.which;
-  var charStr = String.fromCharCode(charCode);
-  if (!charStr.match(/^[\d|\.|\,]+/))
-    e.preventDefault();
-}
+    <form action="/action_page.php">
+      <label for="quantity">Quantity (between 1 and 5):</label>
+      <input
+        type="text"
+        id="quantity"
+        name="quantity"
+        onkeypress="validate(event, this)"
+      />
+      <input type="submit" />
+    </form>
+    <script>*/
+      function validate(evt, ele) {
+        let theEvent = evt || window.event;
+        let key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+        let value = ele.value + key;
+        let regex = /^\d+(,\d{0,2})?$/;
+        if (!regex.test(value)) {
+          theEvent.returnValue = false;
+          if (theEvent.preventDefault) theEvent.preventDefault();
+        }
+      }
+      /*
+    </script>
+  </body>
+</html>
+*/
